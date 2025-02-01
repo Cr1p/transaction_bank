60b02b4c78ad3c2e11be85c96de59492adc59554
andy cg <acastillo.g@hotmail.com>
2025-01-31 19:02:59 -0500

    uploading non-added files

---
+++ src/test/java/com/nttdata/transactionBank/TransactionBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.transactionBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
